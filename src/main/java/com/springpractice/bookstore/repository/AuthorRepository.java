84bfa097ded8b9983d2404af6fe6163c3881f980
yousefwmorsy <yousefwmorsy@gmail.com>
2026-07-08 00:47:34 +0300

    feat(author): add author repository

---
+++ src/main/java/com/springpractice/bookstore/repository/AuthorRepository.java
@@ -0,0 +1,9 @@
+package com.springpractice.bookstore.repository;
+
+import com.springpractice.bookstore.model.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+}
