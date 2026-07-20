54f72136eecc2bfcffb46b444b5930e26befa025
yousefwmorsy <yousefwmorsy@gmail.com>
2026-07-20 22:12:18 +0300

    fix(ci): fix injected error

--- src/main/java/com/springpractice/bookstore/mapper/AuthorMapper.java
+++ src/main/java/com/springpractice/bookstore/mapper/AuthorMapper.java
@@ -23,6 +23,6 @@ public class AuthorMapper {
         author.setFirstName(dto.firstName());
         author.setLastName(dto.lastName());
         author.setBiography(dto.biography());
-        return new Author();
+        return author;
     }
 }
